f4a3d1ec851921573e22d923a3994e2b78e643e0
johnchower <johnchower@gmail.com>
2017-01-16 14:58:22 -0700

    Document retention rate queries.

---
+++ R/data_doc.r
@@ -0,0 +1,10 @@
+#' A string containing the user retention query.
+#' 
+#' @format A length-one character vector.
+"query_retention"
+
+#' A string containing the user retention query with a placeholder for
+#' subsetting.
+#'
+#' @format A length-one character vector.
+"query_retention_sub"
