6d1ecd264e729402e93c5cfb6e98b6fbc3370c5c
johnchower <johnchower@gmail.com>
2017-01-23 14:05:51 -0700

    Use a sample to determine height, because plotting a dendrogram of 40000 users is not feasible.

--- inst/scripts/initial_correlation.r
+++ inst/scripts/initial_correlation.r
@@ -1,9 +1,27 @@
+library(dplyr)
+
 # Connect to Redshift and create temporary tables user_flash_cat and pa_flash_cat.
 glootility::connect_to_redshift()
 RPostgreSQL::dbGetQuery(conn = redshift_connection$con
                         , statement = glootility::query_pa_flash_cat)
 RPostgreSQL::dbGetQuery(conn = redshift_connection$con
                         , statement = glootility::query_user_flash_cat)
+
+# Take a sample of the set of all users on the platform.
+sampleUserSet <- 
+  RPostgreSQL::dbGetQuery(conn = redshift_connection$con
+                          , statement = "select user_id as id from user_flash_cat")  %>%
+  {slice(., sample(1:nrow(.), size = 500))} %>%
+  {.$id}
+
+# Calculate the platform action distribution for the sample set, in their first hour
+# on the platform.
+sampleUserPADist <- 
+  oneD7::calculatePADist(users = sampleUserSet)
+
+sampleUserClust <- clusterUsers(sampleUserPADist)
+plot(sampleUserClust) #Use the dendrogram to choose a suitable height.
+H <- 7
 
 # Calculate the platform action distribution for all users, in their first hour
 # on the platform.
@@ -12,12 +30,11 @@ allUserPADist <-
 
 # Cluster all users according to their hour-1 platform action distribution.
 allUserClust <- clusterUsers(allUserPADist)
-plot(allUserClust)
 
 # Calculate aggregate platform action distributions for each cluster.
 aggPADistList <- clustApply(
   hclustObject=allUserClust
-  , height = 4
+  , height = H
   , FUN = function(u){
       dplyr::select(calculatePADist(u, agg = T)
                     , flash_report_category, pct_platform_actions)
@@ -25,7 +42,7 @@ aggPADistList <- clustApply(
 
 # Calculate long-term retention numbers for each cluster. 
 retentionList <- clustApply(hclustObject=allUserClust
-                            , height = 4
+                            , height = H
                             , FUN = calculateWeeklyRetention)
 
 # Plot long-term retention numbers for each cluster
