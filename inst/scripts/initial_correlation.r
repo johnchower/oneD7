8259bc8a7b5fce1953d9e8a7f21973b1640b4db9
johnchower <johnchower@gmail.com>
2017-01-24 12:59:07 -0700

    Further work on initial_correlation script.

--- inst/scripts/initial_correlation.r
+++ inst/scripts/initial_correlation.r
@@ -1,4 +1,6 @@
+library(ggplot2)
 library(dplyr)
+library(plotly)
 
 # Connect to Redshift and create temporary tables user_flash_cat and pa_flash_cat.
 glootility::connect_to_redshift()
@@ -6,45 +8,45 @@ RPostgreSQL::dbGetQuery(conn = redshift_connection$con
                         , statement = glootility::query_pa_flash_cat)
 RPostgreSQL::dbGetQuery(conn = redshift_connection$con
                         , statement = glootility::query_user_flash_cat)
-
-# Take a sample of the set of all users on the platform.
-sampleUserSet <- 
-  RPostgreSQL::dbGetQuery(conn = redshift_connection$con
-                          , statement = "select user_id as id from user_flash_cat")  %>%
-  {slice(., sample(1:nrow(.), size = 500))} %>%
-  {.$id}
-
-# Calculate the platform action distribution for the sample set, in their first hour
-# on the platform.
-sampleUserPADist <- 
-  oneD7::calculatePADist(users = sampleUserSet)
-
-sampleUserClust <- clusterUsers(sampleUserPADist)
-plot(sampleUserClust) #Use the dendrogram to choose a suitable height.
-H <- 7
-
 # Calculate the platform action distribution for all users, in their first hour
 # on the platform.
 allUserPADist <- 
-  oneD7::calculatePADist()
+  oneD7::calculatePADist() 
+
+sampleUserPADist <- allUserPADist %>%
+  slice(sample(1:nrow(.), size = 1000))
 
 # Cluster all users according to their hour-1 platform action distribution.
 allUserClust <- clusterUsers(allUserPADist)
+sampleUserClust <- clusterUsers(sampleUserPADist)
+
+K <- 4
 
 # Calculate aggregate platform action distributions for each cluster.
 aggPADistList <- clustApply(
   hclustObject=allUserClust
-  , height = H
+  , num_clusters = K
   , FUN = function(u){
       dplyr::select(calculatePADist(u, agg = T)
                     , flash_report_category, pct_platform_actions)
+    }
 )
+
+# Display aggregate platform action distributions for each cluster
+.env$view(squashPADistList(aggPADistList))
 
 # Calculate long-term retention numbers for each cluster. 
 retentionList <- clustApply(hclustObject=allUserClust
-                            , height = H
+                            , num_clusters = K
                             , FUN = calculateWeeklyRetention)
 
 # Plot long-term retention numbers for each cluster
-
+retentionData <- squashRetentionList(retentionList)
+x <- retentionData %>%
+  filter(relative_session_week > 0) %>%
+  mutate(cluster = as.character(cluster)) %>%
+  ggplot(aes(x=relative_session_week, y=pct_active, color = cluster, group = cluster)) +
+  geom_line() 
+ggplotly(x)
+  
 RPostgreSQL::dbDisconnect(conn = redshift_connection$con)
